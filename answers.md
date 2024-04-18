aaa0a898e72eec28d87bedfbbb4c2a928775e536
nicoledavis203 <127431834+nicoledavis203@users.noreply.github.com>
2024-04-18 03:32:44 +0000

    Final

--- answers.md
+++ answers.md
@@ -8,10 +8,10 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 
 
-- **2)**
+- **2)** Work= O(n+m) 
 
-- **4)**
+- **4)**Worst case for calling reachable would be for a complete graph where every node has all neightbors, which would be O(n^2)
 
-- **5)**
+- **5)** Work is O(n^2)
 
-- **7)**
+- **7)** Does not change complexity/work will remain 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 
+
+
 def make_undirected_graph(edge_list):
     """ Makes an undirected graph from a list of edge tuples. """
     graph = defaultdict(set)
@@ -7,6 +9,7 @@ def make_undirected_graph(edge_list):
         graph[e[0]].add(e[1])
         graph[e[1]].add(e[0])
     return graph
+
 
 
 def reachable(graph, start_node):
@@ -17,17 +20,36 @@ def reachable(graph, start_node):
     result = set([start_node])
     frontier = set([start_node])
     while len(frontier) != 0:
-        ###TODO
-        pass
+        new_frontier = set()
+        for node in frontier:
+            neighbors = graph[node]
+            for neighbor in neighbors:
+                if neighbor not in result:
+                    new_frontier.add(neighbor)
+                    result.add(neighbor)
+        frontier = new_frontier
     return result
 
 
 
 
-
 def connected(graph):
-    ### TODO
-    pass
+    nodes = set(graph.keys())
+    if len(nodes) == 0:
+        return False
+
+    visited = set()
+    stack = [next(iter(nodes))]
+
+    while stack:
+        node = stack.pop()
+        visited.add(node)
+        neighbors = graph[node]
+        for neighbor in neighbors:
+            if neighbor not in visited:
+                stack.append(neighbor)
+
+    return len(visited) == len(nodes)
 
 
 
@@ -37,6 +59,19 @@ def n_components(graph):
     Returns:
       the number of connected components in an undirected graph
     """
-    ### TODO
-    pass
+    visited = set()
+    components = 0
+
+    def dfs(node):
+        visited.add(node)
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                dfs(neighbor)
+
+    for node in graph:
+        if node not in visited:
+            components += 1
+            dfs(node)
+    
+    return components
 
